627af7ae840ea8976642b53b6666aa5a69cf5931
JulioAco <94719915+JulioAco@users.noreply.github.com>
2024-10-31 14:20:32 +0100

    downloads-curl.js
    v2.0

--- downloads-curl.js
+++ downloads-curl.js
@@ -102,7 +102,7 @@ async function handleDownload() {
                     } else {
                         countdownElement.textContent = `Espere ${countdown} segundos...`;
                     }
-                }, 10000);
+                }, 1000);
             }
             break; // Salimos del bucle después de encontrar el radio seleccionado
         }
